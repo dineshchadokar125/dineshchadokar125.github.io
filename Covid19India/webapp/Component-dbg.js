3baf33f2263dd4ae81cde808c95dc8035dfec8aa
Dinesh Chadoker <dineshchadokar125@gmail.com>
2020-04-29 12:02:18 +0530

    Add files via upload

--- Covid19India/webapp/Component-dbg.js
+++ Covid19India/webapp/Component-dbg.js
@@ -1,1 +1,71 @@
-sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","com/sap/dinesh/covid19india/dashboard/model/models","com/sap/dinesh/covid19india/dashboard/controller/ErrorHandler","sap/base/Log"],function(s,t,e,i,o){"use strict";return s.extend("com.sap.dinesh.covid19india.dashboard.Component",{metadata:{manifest:"json"},init:function(){s.prototype.init.apply(this,arguments),o.Level="NONE",this._oErrorHandler=new i(this),this.setModel(e.createDeviceModel(),"device"),this.getRouter().initialize()},destroy:function(){this._oErrorHandler.destroy(),s.prototype.destroy.apply(this,arguments)},getContentDensityClass:function(){return void 0===this._sContentDensityClass&&(jQuery(document.body).hasClass("sapUiSizeCozy")||jQuery(document.body).hasClass("sapUiSizeCompact")?this._sContentDensityClass="":t.support.touch?this._sContentDensityClass="sapUiSizeCozy":this._sContentDensityClass="sapUiSizeCompact"),this._sContentDensityClass}})});
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"com/sap/dinesh/covid19india/dashboard/model/models",
+	"com/sap/dinesh/covid19india/dashboard/controller/ErrorHandler",
+	"sap/base/Log"
+], function (UIComponent, Device, models, ErrorHandler, Log) {
+	"use strict";
+
+	return UIComponent.extend("com.sap.dinesh.covid19india.dashboard.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * In this function, the device models are set and the router is initialized.
+		 * @public
+		 * @override
+		 */
+		init: function () {
+			// call the base components init function
+			UIComponent.prototype.init.apply(this, arguments);
+			Log.Level = "NONE";
+			// initialize the error handler with the component
+			this._oErrorHandler = new ErrorHandler(this);
+
+			// set the device model
+			this.setModel(models.createDeviceModel(), "device");
+
+			// create the views based on the url/hash
+			this.getRouter().initialize();
+		},
+
+		/**
+		 * The component is destroyed by UI5 automatically.
+		 * In this method, the ErrorHandler is destroyed.
+		 * @public
+		 * @override
+		 */
+		destroy: function () {
+			this._oErrorHandler.destroy();
+			// call the base components destroy function
+			UIComponent.prototype.destroy.apply(this, arguments);
+		},
+
+		/**
+		 * This method can be called to determine whether the sapUiSizeCompact or sapUiSizeCozy
+		 * design mode class should be set, which influences the size appearance of some controls.
+		 * @public
+		 * @return {string} css class, either 'sapUiSizeCompact' or 'sapUiSizeCozy' - or an empty string if no css class should be set
+		 */
+		getContentDensityClass: function () {
+			if (this._sContentDensityClass === undefined) {
+				// check whether FLP has already set the content density class; do nothing in this case
+				if (jQuery(document.body).hasClass("sapUiSizeCozy") || jQuery(document.body).hasClass("sapUiSizeCompact")) {
+					this._sContentDensityClass = "";
+				} else if (!Device.support.touch) { // apply "compact" mode if touch is not supported
+					this._sContentDensityClass = "sapUiSizeCompact";
+				} else {
+					// "cozy" in case of touch support; default for most sap.m controls, but needed for desktop-first controls like sap.ui.table.Table
+					this._sContentDensityClass = "sapUiSizeCozy";
+				}
+			}
+			return this._sContentDensityClass;
+		}
+
+	});
+
+});
